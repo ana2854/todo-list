4708bcc409fe1df93fc894c75bdd97963c9a9227
Ana <140516434+ana2854@users.noreply.github.com>
2023-12-06 13:26:07 +0100

    ajout note

---
+++ script.js
@@ -0,0 +1,22 @@
+//ajout note
+
+const form = document.querySelector("#new-todo-form");
+const todoInput = document.querySelector("#todo-input");
+const list = document.querySelector("#list");
+const template = document.querySelector("#list-item-template");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const todoName = todoInput.value;
+  displayTodo(todoName);
+  todoInput.value = "";
+});
+
+function displayTodo(todoName) {
+  const templateClone = template.content.cloneNode(true);
+  const textElement = templateClone.querySelector("[data-list-item-text]");
+  textElement.innerText = todoName;
+  list.appendChild(textElement);
+  console.log(templateClone);
+}
