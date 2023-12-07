a6135150b863018021814055840cbd821489f85d
Ana <140516434+ana2854@users.noreply.github.com>
2023-12-07 10:49:17 +0100

    storage todos

--- script.js
+++ script.js
@@ -4,12 +4,18 @@ const form = document.querySelector("#new-todo-form");
 const todoInput = document.querySelector("#todo-input");
 const list = document.querySelector("#list");
 const template = document.querySelector("#list-item-template");
+const todos = []
+LOCAL_STORAGE_PREFIX = "TODO_LIST";
+TODOS_STORAGE_KEY = `${LOCAL_STORAGE_PREFIX}-todos`
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const todoName = todoInput.value;
+  if(todoName === "") return 
+  todos.push(todoName)
   displayTodo(todoName);
+  saveTodos()
   todoInput.value = "";
 });
 
@@ -19,4 +25,8 @@ function displayTodo(todoName) {
   textElement.innerText = todoName;
   list.appendChild(templateClone);
   console.log(templateClone);
+}
+
+function saveTodos () {
+   localStorage.setItem(TODOS_STORAGE_KEY,JSON.stringify(todos))
 }
