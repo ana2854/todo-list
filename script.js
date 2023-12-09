3edbd0b0054fca3fd596675ad6239c5e2f551dd9
Ana <140516434+ana2854@users.noreply.github.com>
2023-12-09 12:09:46 +0100

    affichage persistant des données

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
 //ajout note
 
-const form = document.querySelector("#new-todo-form");
-const todoInput = document.querySelector("#todo-input");
-const list = document.querySelector("#list");
-const template = document.querySelector("#list-item-template");
-const todos = []
-LOCAL_STORAGE_PREFIX = "TODO_LIST";
+const form = document.querySelector("#new-todo-form")
+const todoInput = document.querySelector("#todo-input")
+const list = document.querySelector("#list")
+const template = document.querySelector("#list-item-template")
+LOCAL_STORAGE_PREFIX = "TODO_LIST"
 TODOS_STORAGE_KEY = `${LOCAL_STORAGE_PREFIX}-todos`
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
+const todos = loadTodos()
+todos.forEach(displayTodo)
 
-  const todoName = todoInput.value;
-  if(todoName === "") return 
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+
+  const todoName = todoInput.value
+  if (todoName === "") return
   todos.push(todoName)
-  displayTodo(todoName);
+  displayTodo(todoName)
   saveTodos()
-  todoInput.value = "";
-});
+  todoInput.value = ""
+})
 
 function displayTodo(todoName) {
-  const templateClone = template.content.cloneNode(true);
-  const textElement = templateClone.querySelector("[data-list-item-text]");
-  textElement.innerText = todoName;
-  list.appendChild(templateClone);
-  console.log(templateClone);
+  const templateClone = template.content.cloneNode(true)
+  const textElement = templateClone.querySelector("[data-list-item-text]")
+  textElement.innerText = todoName
+  list.appendChild(templateClone)
 }
 
-function saveTodos () {
-   localStorage.setItem(TODOS_STORAGE_KEY,JSON.stringify(todos))
+function loadTodos() {
+  const todosString = localStorage.getItem(TODOS_STORAGE_KEY)
+  return JSON.parse(todosString) || []
+}
+
+function saveTodos() {
+  localStorage.setItem(TODOS_STORAGE_KEY, JSON.stringify(todos))
 }
